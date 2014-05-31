a0956120fb7244e2efeac3d9c8c0d4d3a78b0a48
Sirius <oma9512@yandex.ru>
2014-05-31 16:05:42 +0400

    initial

---
+++ config/pagination.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+return array(
+	'default' => array(
+		"list_params" => array("class"=>"pagination"),
+		"show_in_block" => false,
+		"switch_type" => "pieces",
+		"lang_next" => "Вперед",
+		"lang_prev" => "Назад",
+		"left" => 2,
+		"right" => 2,
+	)
+);
